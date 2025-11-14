90d69b7086369b324ca9e1e5808ddc2e816feda6
Arjun-commit <kumararjunsh@gmail.com>
2025-11-13 17:16:58 -0700

    router_change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Layout from "./Layout.js";
 import Portfolio from "./Pages/Portfolio";
 
